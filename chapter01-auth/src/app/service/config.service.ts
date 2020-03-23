0b1e3bb79eaaa9ec0f936ece344346241919692e
cherryApp <j.cserko@gmail.com>
2020-03-23 18:39:15 +0100

    Create chapter-01-05 branch

--- chapter01-auth/src/app/service/config.service.ts
+++ chapter01-auth/src/app/service/config.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
 
+  apiUrl = `http://localhost:3000/`;
+
   navigation: {label: string, href: string, role: number}[] = [
     {label: 'Home', href: '', role: 1},
     {label: 'Users', href: '/users', role: 2},
---
+++ chapter01-auth/src/app/service/user.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  entity = 'users';
+
+  constructor(
+    private http: HttpClient,
+    private config: ConfigService,
+  ) { }
+
+  get(id?: string | number): Observable<User | User[]> {
+    let url = `${this.config.apiUrl}${this.entity}`;
+    if (id) {
+      url += `=${id}`;
+    }
+
+    return this.http.get<User[]>(url);
+  }
+
+  query(queryString: string): Observable<User | User[]> {
+    const url = `${this.config.apiUrl}${this.entity}?${queryString}`;
+    return this.http.get<User[]>(url);
+  }
+
+  update(user: User): Observable<User> {
+    const url = `${this.config.apiUrl}${this.entity}?${user.id}`;
+    return this.http.put<User>(url, user);
+  }
+}
