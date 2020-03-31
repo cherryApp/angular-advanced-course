7ce20da434763de3b17a4cf9f033824a8fff3bae
cherryApp <j.cserko@gmail.com>
2020-03-31 15:01:40 +0200

    Create course-9-15 branch

--- chapter02-perf/src/app/service/auth.service.ts
+++ chapter02-perf/src/app/service/auth.service.ts
@@ -24,7 +24,12 @@ export class AuthService {
     private http: HttpClient,
     private router: Router,
     private userService: UserService,
-  ) { }
+  ) {
+    const storedUser = localStorage.getItem(this.storageName);
+    if (storedUser) {
+      this.currentUserSubject.next(JSON.parse(storedUser));
+    }
+  }
 
   get currentUserValue(): User {
     return this.currentUserSubject.value;
