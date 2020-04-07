d3d90a13be48d37e2b65f3c463b94765b2f865ce
cherryApp <j.cserko@gmail.com>
2020-04-07 16:52:53 +0200

    Create course-9-32 branch

---
+++ chapter04-material/src/app/model/user.ts
@@ -0,0 +1,13 @@
+// tslint:disable: variable-name
+export class User {
+  id?: number;
+  first_name?: string;
+  last_name?: string;
+  email?: string;
+  gender?: string;
+  address?: string;
+  editable?: boolean;
+  active?: boolean;
+  onBoarding?: string;
+  salary?: number;
+}
