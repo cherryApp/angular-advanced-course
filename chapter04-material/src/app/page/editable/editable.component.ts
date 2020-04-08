2c37a2ad9a514c31c08592c617e7f3d55ff1ee8d
cherryApp <j.cserko@gmail.com>
2020-04-08 11:16:39 +0200

    Create course-9-39 branch

--- chapter04-material/src/app/page/editable/editable.component.ts
+++ chapter04-material/src/app/page/editable/editable.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { User } from 'src/app/model/user';
+import { Subscription } from 'rxjs';
+import { MatPaginator } from '@angular/material/paginator';
+import { UserService } from 'src/app/service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editable',
   templateUrl: './editable.component.html',
   styleUrls: ['./editable.component.scss']
 })
-export class EditableComponent implements OnInit {
+export class EditableComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  dataSource: MatTableDataSource<User> = new MatTableDataSource<User>();
+  displayedColumns: string[] = [
+    'id',
+    'first_name',
+    'last_name',
+    'gender',
+    'email',
+    'address',
+    'actions',
+  ];
+  pageSizes: number[] = [5, 10, 25, 100];
+  dataSubscription: Subscription;
+  currentFilterKey: string;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+  ) { }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSubscription = this.userService.watcher$.subscribe(
+      users => this.dataSource.data = (users as unknown as User[])
+    );
+    this.userService.refresh();
+
+    this.dataSource.filterPredicate = (data: User, filter: string) => {
+      const key = this.currentFilterKey || '';
+      const source = key ? String(data[key]) : JSON.stringify(data);
+      return source.toLowerCase().includes(filter.toLowerCase());
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.dataSubscription.unsubscribe();
+  }
+
+  onEdit(user: User): void {
+    this.router.navigate(['editable', 'edit', user.id]);
+  }
+
+  onDelete(user: User): void {
+    this.userService.delete(user.id).toPromise().then(
+      response => console.log(response),
+      err => console.error(err)
+    );
   }
 
 }
